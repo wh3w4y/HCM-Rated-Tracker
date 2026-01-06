49fefa8def2bfa948ed7bb61ebbb0318b51287e8
wh3w4y <98826596+wh3w4y@users.noreply.github.com>
2026-01-06 16:05:50 +0000

    Update const.py

--- custom_components/hcm_rated_tracker/const.py
+++ custom_components/hcm_rated_tracker/const.py
@@ -1,12 +1,19 @@
 from __future__ import annotations
 
+from homeassistant.const import Platform
+
 DOMAIN = "hcm_rated_tracker"
 
 ATTR_TITLE = "title"
 ATTR_EXTRA = "extra"
 ATTR_RATING = "rating"
 
-# YAML source of truth (relative to /config)
+# YAML source of truth (relative to HA config folder)
+# (For you this is effectively /homeassistant)
 DEFAULT_YAML_REL_PATH = "hcm_rated_tracker/books.yaml"
 
-PLATFORMS: list[str] = ["text", "number", "button"]
+PLATFORMS: list[Platform] = [
+    Platform.TEXT,
+    Platform.NUMBER,
+    Platform.BUTTON,
+]
